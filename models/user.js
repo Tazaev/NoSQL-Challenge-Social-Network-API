b7cb15a2d9c232eda7f6219526f0d2a83a10c1ce
Tazaev <tonymurtazaev@gmail.com>
2022-07-26 21:46:05 -0400

    added schema

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const userData = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userData.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", userData);
+
+module.exports = User;
